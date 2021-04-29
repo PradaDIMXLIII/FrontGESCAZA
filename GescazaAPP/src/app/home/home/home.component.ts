653b09dda80a11cbfead285facb2c1e62a36e8b7
Miguel Prada <Alkoliahmpm.mp@gmail.com>
2021-04-29 22:57:36 +0200

    "modificando core"

--- GescazaAPP/src/app/home/home/home.component.ts
+++ GescazaAPP/src/app/home/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: []
+  styles: ['img { height: 100%; width: 100%; border-radius: 50%; opacity: 0.5 }']
 })
 export class HomeComponent implements OnInit {
 
