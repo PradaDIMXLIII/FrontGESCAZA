52bbd0175bbf6eab5aa60899e6ccf8f9270a5b3d
Miguel Prada <Alkoliahmpm.mp@gmail.com>
2021-05-02 21:14:01 +0200

    "modificando header"

--- GescazaAPP/src/app/core/shell/header/header.component.ts
+++ GescazaAPP/src/app/core/shell/header/header.component.ts
@@ -4,7 +4,36 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styles: [`.jumbotron {
+    padding: 5px;
+    margin: 1px;
+    background-image: url('./assets/lobo.jpg');
+    background-position: right bottom;
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    height: 50%;
+    border-radius: 20px;
+  }
+
+  img:hover {
+    transform: scaleX(-1);
+  }
+
+  .examen {
+    text-align: center;
+  }
+
+  p {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }`
+  ]
 })
 export class HeaderComponent implements OnInit {
 
